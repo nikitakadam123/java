305366d07ed2910ccf9e78fe630ef928931ecff4
Nikita Kadam <114174410+nikitakadam123@users.noreply.github.com>
2023-11-02 11:19:52 -0700

    Add files via upload

---
+++ EmployeeCurd/src/Employee.java
@@ -0,0 +1,25 @@
+import java.util.*;
+class Employee {
+    private int empno;
+    private String ename;
+    private int salary;
+    Employee(int empno, String ename, int salary) {
+        this.empno = empno;
+        this.ename = ename;
+        this.salary = salary;
+    }
+    public int getEmpno() {
+        return empno;
+    }
+    public int getSalary() {
+        return salary;
+    }
+    public String getEname() {
+        return ename;
+    }
+    public String toString() {
+        return empno + " " + ename + " " + salary;
+    }
+}
+
+
